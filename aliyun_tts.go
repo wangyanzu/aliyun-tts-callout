8c30ec0220465e57cacf26593cf7f5c3c1cb6ef4
汪彦祖 <wangsheng@xiaoliuapp.com>
2023-04-26 13:32:42 +0800

    去除重复标记

--- aliyun_tts.go
+++ aliyun_tts.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aliyun_tts_callout/callout"
 	"flag"
+	"fmt"
 	"log"
 	"strings"
 )
@@ -12,14 +13,15 @@ func main() {
 	accessKeyId := flag.String("ak", "", "aliyun accessKeyId")
 	accessKeySecret := flag.String("as", "", "aliyun accessKeySecret")
 	ttsCode := flag.String("t", "", "tts code, TTS_xxxxxxxxxx")
-	showNumber := flag.String("t", "", "CalledShowNumber")
-	param := flag.String("b", `{"level":"未知","value":"0","rulename":"xx告警"}`, "tts param")
+	showNumber := flag.String("n", "", "CalledShowNumber")
+	param := flag.String("b", `{"level":"测试","value":"666","rulename":"测试告警"}`, "tts param")
 	phone := flag.String("p", "", "phone list, 135xxx,136xxx,137xxx")
 	dryrun := flag.Bool("d", false, "do nothing")
 
 	flag.Parse()
 	if *phone == "" {
-		panic("phone not found")
+		fmt.Println("phone not found")
+		flag.Parsed()
 	}
 	phones := strings.Split(*phone, ",")
 
